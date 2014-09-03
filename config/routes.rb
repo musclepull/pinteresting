1c1e49f233a59ec073ee281844d8095cbec71a75
agopinat <musclepull@gmail.com>
2014-09-03 01:45:15 -0400

    Added some front end stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get "about" => "pages#about"
   get "contact" => "pages#contact"
+  get "privacy" => "pages#privacy"
+  get "terms" => "pages#terms"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
